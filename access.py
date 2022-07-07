dca8ebe96edc16ff3b0c1e97370f4a9a0d7376ea
ailinvenerus <ailinvenerus@gmail.com>
2022-07-07 12:19:48 +0100

    Adding missing methods and Tokens structure

--- access.py
+++ access.py
@@ -1,79 +1,117 @@
-from io import TextIOWrapper
 from typing import List
 import boto3
+import botocore
+from botocore.config import Config
 
-# TODO: RETURN DICTIONARIES FOR ALL METHODS
+config = Config(signature_version=botocore.UNSIGNED)
+clientUnsigned = boto3.client(
+    'cognito-idp', config=config, region_name='eu-west-2')  # only for revoke_tokens
+clientSigned = boto3.client(
+    'cognito-idp', region_name='eu-west-2')
+# TODO: CREATE OBJECT CLIENT TO EXECUTE METHODS
+# TODO: TEST ALL
 
-client = boto3.client('cognito-idp', region_name='eu-west-2')
+
+class Tokens:
+    def __init__(self, refreshToken, accessToken, idToken):
+        self.tokens = {
+            'REFRESH_TOKEN': refreshToken,
+            'ACCESS_TOKEN': accessToken,
+            'ID_TOKEN': idToken
+        }
 
 
-def change_password(clientId: str, username: str, password: str, newPassword: str):
-    success = "Password successfully changed"
+class CognitoAccess:
+    def __init__(self, clientUnsigned, clientSigned):
+        self.clientUnsigned = clientUnsigned
+        self.clientSigned = clientSigned
+
+
+def change_password(clientId: str, username: str, password: str, newPassword: str) -> bool:
     try:
         responseInitiate = initiate_auth(
             clientId, username, password)
-    except Exception as e:
-        raise Exception(e)
+    except Exception:
+        return False
     challengeName = responseInitiate.get('ChallengeName')
     if challengeName == None:
         try:
             authResult = responseInitiate.get('AuthenticationResult')
             accessToken = authResult.get('AccessToken')
-            response = change_password_cognito(
+            change_password_cognito(
                 password, newPassword, accessToken)
-            metaData = response.get('ResponseMetadata')
-            statusCode = metaData.get('HTTPStatusCode')
-            if statusCode == 200:
-                print(success)
-                return response
-            else:
-                print(response)
-                raise Exception(
-                    "Request failed when trying to change password")
-        except Exception as e:
-            raise Exception(e)
+            return True
+        except Exception:
+            return False
     else:
         session = responseInitiate.get("Session")
         challengeParameters = responseInitiate.get("ChallengeParameters")
         userIdSRP = challengeParameters.get("USER_ID_FOR_SRP")
         requiredAttributes = challengeParameters.get("requiredAttributes")
         try:
-            response = respond_to_auth_challenge(
+            respond_to_auth_challenge(
                 clientId, username, newPassword, session, userIdSRP, requiredAttributes)
-            print(success)
-            return response
-        except Exception as e:
-            raise Exception(e)
+            return True
+        except Exception:
+            return False
 
 
-# When a ConfirmationCode is received:
-def forgot_password(clientId: str, username: str, password: str, confirmationCode: str):
+def forgot_password(clientId: str, username: str, password: str, confirmationCode: str) -> str:
     try:
-        response = client.confirm_forgot_password(
+        clientSigned.confirm_forgot_password(
             ClientId=clientId,
             Username=username,
             ConfirmationCode=confirmationCode,
             Password=password
         )
-        return response
+        return "OK"
     except Exception:
-        return "Data entered is incorrect"
+        raise Exception(
+            "UnauthorizedError: Invalid parameters. Could not reset password")
 
 
-def get_tokens(clientId: str, username: str, password: str):
+def get_tokens(clientId: str, **param) -> Tokens:
+    authResult = 'AuthenticationResult'
+    refreshToken = param.get('REFRESH_TOKEN')
+    if refreshToken == None:
+        username = param.get('USERNAME')
+        password = param.get('PASSWORD')
+        if username != None and password != None:
+            response = initiate_auth(clientId, username, password)
+            result = response.get(authResult)
+            refreshToken = result.get('RefreshToken')
+            accessToken = result.get('AccessToken')
+            idToken = result.get('IdToken')
+            return Tokens(refreshToken, accessToken, idToken)
+        else:
+            raise Exception(
+                "UnauthorizedError: Invalid parameters. Could not get tokens")
+    else:
+        response = get_tokens_from_refresh(clientId, refreshToken)
+        result = response.get(authResult)
+        accessToken = result.get('AccessToken')
+        idToken = result.get('IdToken')
+        return Tokens(refreshToken, accessToken, idToken)
+
+
+def revoke_and_get_new_tokens(clientId: str, refreshToken: str, username: str, password: str):
+    response = revoke_token(clientId, refreshToken)
     try:
-        response = initiate_auth(clientId, username, password)
-        challengeName = response.get('ChallengeName')
-        if challengeName == None:
-            return response
-    except Exception:
-        raise Exception("Please change your password")
+        response = initiate_auth(
+            clientId, username, password)
+        result = response.get('AuthenticationResult')
+        refreshToken = result.get('RefreshToken')
+        accessToken = result.get('AccessToken')
+        idToken = result.get('IdToken')
+        return Tokens(refreshToken, accessToken, idToken)
+    except:
+        raise Exception("UnauthorizedError: Invalid username or password")
 
-    # --------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
 
 
 def initiate_auth(clientId: str, username: str, password: str) -> str:
-    response = client.initiate_auth(
+    response = clientSigned.initiate_auth(
         ClientId=clientId,
         AuthFlow='USER_PASSWORD_AUTH',
         AuthParameters={
@@ -83,7 +121,7 @@ def initiate_auth(clientId: str, username: str, password: str) -> str:
 
 
 def respond_to_auth_challenge(clientId: str, username: str, newPassword: str, session: str, userIdSRP: str, requiredAttributes: List[str]):
-    response = client.respond_to_auth_challenge(
+    response = clientSigned.respond_to_auth_challenge(
         ClientId=clientId,
         ChallengeName='NEW_PASSWORD_REQUIRED',
         Session=session,
@@ -97,8 +135,8 @@ def respond_to_auth_challenge(clientId: str, username: str, newPassword: str, se
     return response
 
 
-def get_access_token(clientId: str, refreshToken: str) -> str:
-    response = client.initiate_auth(
+def get_tokens_from_refresh(clientId: str, refreshToken: str) -> str:
+    response = clientSigned.initiate_auth(
         ClientId=clientId,
         AuthFlow='REFRESH_TOKEN',
         AuthParameters={
@@ -108,28 +146,15 @@ def get_access_token(clientId: str, refreshToken: str) -> str:
 
 
 def change_password_cognito(password: str, newPassword: str, accessToken: str):
-    response = client.change_password(
+    response = clientSigned.change_password(
         PreviousPassword=password,
         ProposedPassword=newPassword,
         AccessToken=accessToken)
     return response
 
 
-# TODO: PENDING
-def revoke_token(clientId: str):
-    print(clientId)
-
-
-def read_next(file: TextIOWrapper):
-    return file.readline().rstrip()
-
-
-f = open("credentials.txt", "r")
-clientId: str = read_next(f)
-username: str = read_next(f)
-password: str = read_next(f)
-newPassword: str = read_next(f)
-confirmationCode: str = read_next(f)
-
-# print(change_password(clientId, username, password, newPassword))
-print(get_tokens(clientId, username, password))
+def revoke_token(clientId: str, refreshToken: str):
+    response = clientUnsigned.revoke_token(
+        Token=refreshToken,
+        ClientId=clientId)
+    return response
