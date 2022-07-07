1efd00f5df5001dd8a0b80567d06def3017649de
ailinvenerus <ailinvenerus@gmail.com>
2022-07-07 16:03:13 +0100

    Adding more classes to restrict dictionary types

--- access.py
+++ access.py
@@ -1,26 +1,39 @@
-from typing import List
+from typing import List, TypedDict
 import boto3
 import botocore
 from botocore.config import Config
 
-config = Config(signature_version=botocore.UNSIGNED)
-clientUnsigned = boto3.client(
-    'cognito-idp', config=config, region_name='eu-west-2')
-clientSigned = boto3.client(
-    'cognito-idp', region_name='eu-west-2')
-# TODO: TEST ALL
+
+class Tokens(TypedDict):
+    refreshToken: str
+    accessToken: str
+    idToken: str
+
+
+class RefreshToken(TypedDict):
+    REFRESH_TOKEN: str
+
+
+class UsernamePassword(TypedDict):
+    USERNAME: str
+    PASSWORD: str
 
 
 class CognitoAccess:
 
     def __init__(self, clientId: str):
+        config = Config(signature_version=botocore.UNSIGNED)
+        self.clientUnsigned = boto3.client(
+            'cognito-idp', config=config, region_name='eu-west-2')
+        self.clientSigned = boto3.client(
+            'cognito-idp', region_name='eu-west-2')
         self.clientId = clientId
 
     def change_password(self, username: str, password: str, newPassword: str) -> bool:
         try:
             responseInitiate = self.__initiate_auth(username, password)
         except Exception:
-            return False
+            raise Exception("UnauthorizedError: authentication failed")
         challengeName = responseInitiate.get('ChallengeName')
         if challengeName == None:
             try:
@@ -30,7 +43,8 @@ class CognitoAccess:
                     password, newPassword, accessToken)
                 return True
             except Exception:
-                return False
+                raise Exception(
+                    "UnauthorizedError: password has been changed too many times. Try later")
         else:
             session = responseInitiate.get("Session")
             challengeParameters = responseInitiate.get("ChallengeParameters")
@@ -41,22 +55,22 @@ class CognitoAccess:
                     username, newPassword, session, userIdSRP, requiredAttributes)
                 return True
             except Exception:
-                return False
+                raise Exception("InternalServerError: try again later")
 
-    def forgot_password(self, username: str, password: str, confirmationCode: str) -> str:
+    def forgot_password(self, username: str, password: str, confirmationCode: str) -> bool:
         try:
-            clientSigned.confirm_forgot_password(
+            self.clientSigned.confirm_forgot_password(
                 ClientId=self.clientId,
                 Username=username,
                 ConfirmationCode=confirmationCode,
                 Password=password
             )
-            return "Password successfully updated"
+            return True
         except Exception:
             raise Exception(
                 "UnauthorizedError: Invalid parameters. Could not reset password")
 
-    def get_tokens(self, **param):
+    def get_tokens(self, **param: RefreshToken or UsernamePassword) -> Tokens:
         authResult = 'AuthenticationResult'
         refreshToken = param.get('REFRESH_TOKEN')
         if refreshToken == None:
@@ -66,9 +80,9 @@ class CognitoAccess:
                 response = self.__initiate_auth(username, password)
                 result = response.get(authResult)
                 return {
-                    'REFRESH_TOKEN': result.get('RefreshToken'),
-                    'ACCESS_TOKEN': result.get('AccessToken'),
-                    'ID_TOKEN': result.get('IdToken')
+                    'refreshToken': result.get('RefreshToken'),
+                    'accessToken': result.get('AccessToken'),
+                    'idToken': result.get('IdToken')
                 }
             else:
                 raise Exception(
@@ -78,29 +92,29 @@ class CognitoAccess:
                 response = self.__get_tokens_from_refresh(refreshToken)
                 result = response.get(authResult)
                 return {
-                    'REFRESH_TOKEN': refreshToken,
-                    'ACCESS_TOKEN': result.get('AccessToken'),
-                    'ID_TOKEN': result.get('IdToken')
+                    'refreshToken': refreshToken,
+                    'accessToken': result.get('AccessToken'),
+                    'idToken': result.get('IdToken')
                 }
             except:
                 raise Exception(
                     "UnauthorizedError: Invalid REFRESH_TOKEN. Could not get tokens")
 
-    def revoke_and_get_new_tokens(self, refreshToken: str, username: str, password: str):
+    def revoke_and_get_new_tokens(self, refreshToken: str, username: str, password: str) -> Tokens:
         response = self.__revoke_token(refreshToken)
         try:
             response = self.__initiate_auth(username, password)
             result = response.get('AuthenticationResult')
             return {
-                'REFRESH_TOKEN': result.get('RefreshToken'),
-                'ACCESS_TOKEN': result.get('AccessToken'),
-                'ID_TOKEN': result.get('IdToken')
+                'refreshToken': result.get('RefreshToken'),
+                'accessToken': result.get('AccessToken'),
+                'idToken': result.get('IdToken')
             }
         except:
             raise Exception("UnauthorizedError: Invalid username or password")
 
-    def __initiate_auth(self, username: str, password: str) -> str:
-        response = clientSigned.initiate_auth(
+    def __initiate_auth(self, username: str, password: str):
+        response = self.clientSigned.initiate_auth(
             ClientId=self.clientId,
             AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
@@ -109,7 +123,7 @@ class CognitoAccess:
         return response
 
     def __respond_to_auth_challenge(self, username: str, newPassword: str, session: str, userIdSRP: str, requiredAttributes: List[str]):
-        response = clientSigned.respond_to_auth_challenge(
+        response = self.clientSigned.respond_to_auth_challenge(
             ClientId=self.clientId,
             ChallengeName='NEW_PASSWORD_REQUIRED',
             Session=session,
@@ -122,24 +136,22 @@ class CognitoAccess:
         )
         return response
 
-    def __get_tokens_from_refresh(self, refreshToken: str) -> str:
-        response = clientSigned.initiate_auth(
+    def __get_tokens_from_refresh(self, refreshToken: str):
+        response = self.clientSigned.initiate_auth(
             ClientId=self.clientId,
             AuthFlow='REFRESH_TOKEN',
-            AuthParameters={
-                'REFRESH_TOKEN':
-                    refreshToken})
+            AuthParameters={'REFRESH_TOKEN': refreshToken})
         return response
 
-    def __change_password_cognito(password: str, newPassword: str, accessToken: str):
-        response = clientSigned.change_password(
+    def __change_password_cognito(self, password: str, newPassword: str, accessToken: str):
+        response = self.clientSigned.change_password(
             PreviousPassword=password,
             ProposedPassword=newPassword,
             AccessToken=accessToken)
         return response
 
     def __revoke_token(self, refreshToken: str):
-        response = clientUnsigned.revoke_token(
+        response = self.clientUnsigned.revoke_token(
             Token=refreshToken,
             ClientId=self.clientId)
         return response
