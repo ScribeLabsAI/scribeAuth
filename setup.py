040411c3029f84ddb41f02e4c5e1092572465796
ailinvenerus <ailinvenerus@gmail.com>
2023-08-07 11:31:16 +0100

    build: update library version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
     name='scribeauth',
     python_requires='>=3.10.0',
-    version='1.0.7',
+    version='1.0.8',
     description="Library to authenticate to Scribe's platform",
     long_description=readme(),
     url='https://github.com/ScribeLabsAI/ScribeAuth',
