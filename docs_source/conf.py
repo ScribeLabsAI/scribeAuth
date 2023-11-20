4553d63655975d5223fe73d581b20f133d6e08d7
Ailin Venerus <ailinvenerus@gmail.com>
2023-11-20 11:25:51 +0000

    feat: create documentation to be published in ScribeLabs/documentation repo (#103)
    * fix: complete return type for get_tokens
    
    * feat: improve and transform documentation to md files to be published in ScribeLabs/documentation repo
    
    * fix: improve documentation and automated commit msg
    
    ---------
    
    Co-authored-by: Emmanuel Hadoux <EHadoux@users.noreply.github.com>

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -6,23 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Scribe Auth'
-copyright = '2022, Scribe Labs Limited'
-author = 'Ailin Venerus'
-release = '1.0.2'
+project = "Scribe Auth"
+copyright = "2022, Scribe Labs Limited"
+author = "Ailin Venerus"
+release = "1.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_markdown_builder",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+source_dir = "./scribeauth"
